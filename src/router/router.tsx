55a20ebae8c88c80a5b116c1eb0fb64714638be1
jiwon <leeji.circleu@gmail.com>
2025-07-07 02:10:30 +0900

    lazy 적용 후 Suspense추가

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,7 +1,12 @@
 import { Routes, Route } from "react-router-dom";
-import { lazy, memo } from "react";
+import { lazy, memo, Suspense } from "react";
 import Error from "../views/Error";
 import Index from "../views/Index";
+import Fashion from "../views/Fashion";
+import Cart from "../views/Cart";
+import Products from "../views/Products";
+import Accessory from "../views/Accessory";
+import Digital from "../views/Digital";
 
 const Router = (): JSX.Element => {
   const Products = lazy(() => import("../views/Products"));
@@ -10,16 +15,18 @@ const Router = (): JSX.Element => {
   const Accessory = lazy(() => import("../views/Accessory"));
   const Digital = lazy(() => import("../views/Digital"));
   return (
-    <Routes>
-      <Route path="*" element={<Error />} />
-      <Route path="/" element={<Index />} />
-      {/* 라우팅 추가 해보세요. */}
-      <Route path="/product/:id" element={<Products />} />
-      <Route path="/cart" element={<Cart />} />
-      <Route path="/fashion" element={<Fashion />} />
-      <Route path="/accessory" element={<Accessory />} />
-      <Route path="/digital" element={<Digital />} />
-    </Routes>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="*" element={<Error />} />
+        <Route path="/" element={<Index />} />
+        {/* 라우팅 추가 해보세요. */}
+        <Route path="/product/:id" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/fashion" element={<Fashion />} />
+        <Route path="/accessory" element={<Accessory />} />
+        <Route path="/digital" element={<Digital />} />
+      </Routes>
+    </Suspense>
   );
 };
 
